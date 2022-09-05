f752a8b7c0d7036fc15c6f9f8f67d1d4d13eb735
Joel Fragoso <joelfragoso85@icloud.com>
2022-09-05 20:22:24 -0300

    :white_check_mark: test: verifica LoginRouter
    - verifica se retorna 400 se não for informado um e-mail.

---
+++ tests/presentation/routers/login-router.spec.ts
@@ -0,0 +1,34 @@
+type HttpRequest = {
+  body: {
+    email?: string
+    password?: string
+  }
+}
+
+type HttpResponse = {
+  statusCode: number
+  body?: any
+}
+
+class LoginRouter {
+  route(httpRequest: HttpRequest): HttpResponse | undefined {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse?.statusCode).toBe(400)
+  })
+})
